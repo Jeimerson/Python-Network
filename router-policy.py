2a6557125be4a80af9fd8bfaea842fb75ac47e60
Guerlielton <guerlielton>
2020-08-08 21:18:27 -0300

    Ajuste

--- router-policy.py
+++ router-policy.py
@@ -2,7 +2,7 @@ import re
 import os
 
 def routepolicy(J):
-    arq = open("Route-Policy.txt","w")
+    arq = open('Juniper-Policy.txt','w')
     count = 0
     file = open('asn-list.txt', 'r')
     t = []
@@ -13,16 +13,16 @@ def routepolicy(J):
            if 'AS' in t[i]:   
                 count = count + 1
                 print('set policy-options policy-statement eBGP-DOWNSTREAM-ASN-1234-SPO-IN term {}-V4'.format(t[i]),'from route-filter {} upto /24'.format(t[i+1]),file=arq) 
-                print('set policy-options policy-statement eBGP-DOWNSTREAM-ASN-1234-SPO-IN term {}-V4'.format(t[i]),'from route-filter {} upto /24'.format(t[i+2]),file=arq)
                 print('set policy-options policy-statement eBGP-DOWNSTREAM-ASN-1234-SPO-IN term {}-V4'.format(t[i]),'then local-preference 700',file=arq)
                 print('set policy-options policy-statement eBGP-DOWNSTREAM-ASN-1234-SPO-IN term {}-V4'.format(t[i]),'then community set COSTOMER-ISP',file=arq)
                 print('set policy-options policy-statement eBGP-DOWNSTREAM-ASN-1234-SPO-IN term {}-V4'.format(t[i]),'then community add EXPORT-ONLY-ISP2',file=arq)
                 print('set policy-options policy-statement eBGP-DOWNSTREAM-ASN-1234-SPO-IN term {}-V4'.format(t[i]),'then accept''\n',file=arq)
-                print('------------------------------------------------------------------------------------------------',file=arq)        
-    print('Foram configuradas',count,'Route Policy''\n')
+                print('------------------------------------------------------------------------------------------------',file=arq)
+    print('Junos OS')        
+    print('Foram configuradas',count,'Routing Policy')
 
 def routemap(C):
-    arq = open("Route-Map.txt","w")
+    arq = open('Cisco-Route-Map.txt','w')
     count = 0
     file = open('asn-list.txt', 'r')
     t = []
@@ -33,17 +33,16 @@ def routemap(C):
            if 'AS' in t[i]:   
                 count = count + 1
                 print('ip prefix-list PL-{}-V4'.format(t[i]),'seq 1 permit {} le 24''\n'.format(t[i+1]),file=arq) 
-                print('ip prefix-list PL-{}-V4'.format(t[i]),'seq 2 permit {} le 24''\n'.format(t[i+2]),file=arq)
                 print('route-map eBGP-DOWNSTREAM-ASN-{}-V4-IN'.format(t[i]),'permit 100',file=arq)
                 print('match ip address prefix-list PL-{}-V4'.format(t[i]),file=arq)
                 print('set local-preference 700',file=arq)
                 print('set community 1234:200 1234:243 1234:1023 1234:1043 additive''\n',file=arq)
                 print('------------------------------------------------------------------------------------------------',file=arq)
     print('Cisco IOS-XE')
-    print("Foram configuradas",count,"Route-map")
+    print('Foram configuradas',count,'Route Map')
 
 def route_policy(H):
-    arq = open("Route-Policy.txt","w")
+    arq = open('Huawei-Route-Policy.txt','w')
     count = 0
     file = open('asn-list.txt', 'r')
     t = []
@@ -60,11 +59,11 @@ def route_policy(H):
                 print('apply community 1234:200 1234:243 1234:1023 1234:1043 additive''\n',file=arq)
                 print('------------------------------------------------------------------------------------------------',file=arq)
     print('Huawei VRP')
-    print("Foram configuradas",count,"Route-policy")
+    print('Foram configuradas',count,'Route Policy')
 
 opcao = -1
 while opcao != 0:
-    print('Escolha a Policy a ser configurada')
+    print('Escolha a Policy a ser configurada: ')
     print()
     print('1 - Juniper')
     print('2 - Cisco')
